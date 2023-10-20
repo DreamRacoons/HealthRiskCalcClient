6b1e654fc98471faea61071e15be0e175ac7d9ea
Grover Soans <soansg3445@gmail.com>
2023-10-20 15:21:39 -0500

    .

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 async function start(){
     const response = await fetch("https://health-ins-risk-math.azurewebsites.net/")
     const data = await response.json()
-    createBreedList(data.message)
 }
 
 start()
